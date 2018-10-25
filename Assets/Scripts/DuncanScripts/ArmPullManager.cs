21870486dae3029cbfd427249d5946ca8f8459d9
Alex Duncan <aed428@nyu.edu>
2018-10-25 16:10:06 -0400

    This isn't working right, but it's actually kind of cool so I'm pushing in case we want it for another scene

---
+++ Assets/Scripts/DuncanScripts/ArmPullManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SegmentState{
+	ATTACHED,
+	DETACHED,
+	GRABBED
+}
+
+public class ArmPullManager : MonoBehaviour {
+
+	private static ArmPullManager instance;
+	public static ArmPullManager Instance{
+		get{
+			if(instance == null){
+				instance = FindObjectOfType<ArmPullManager>();
+			}
+			return instance;
+		}
+		set { instance = value; }
+	}
+
+	public float detachDistance;
+	[Range(0,2)] public float pullForce = 1.2f;
+	[Range(0,1)] public float drag = 0.05f;
+	public float maxSegmentDistance = 4f;
+
+	Vector3 rotateMomentum;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,0,rotateMomentum.magnitude);
+	}
+
+	void FixedUpdate(){
+		rotateMomentum *= 0.9f;
+	}
+
+	Vector3 lastMousePos;
+	void OnMouseEnter(){
+		lastMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		lastMousePos.z = 0;
+	}
+	void OnMouseOver(){
+		Debug.Log("HEYEYEYEY");
+		Vector3 worldSpaceMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		worldSpaceMousePos.z = 0;
+
+		if(Vector3.Distance(lastMousePos, worldSpaceMousePos) > 0.1f){
+			rotateMomentum += (worldSpaceMousePos - lastMousePos);
+		}
+		lastMousePos = worldSpaceMousePos;
+	}
+}
--- Assets/Scripts/DuncanScripts/ArmPullScript.cs
+++ Assets/Scripts/DuncanScripts/ArmPullScript.cs
@@ -4,62 +4,116 @@ using UnityEngine;
 
 public class ArmPullScript : MonoBehaviour {
 
-	[SerializeField] bool detached = false;
-	bool grabbed = false;
 
-	[SerializeField] ArmPullScript neighborSegment;
+	[SerializeField] SegmentState currentState = SegmentState.ATTACHED;
+
+	delegate void StateUpdate();
+	StateUpdate stateUpdate;
+
+	void SetState(SegmentState newState){
+		currentState = newState;
+
+		switch(currentState){
+			case SegmentState.ATTACHED:
+				InitAttached();
+				stateUpdate = AttachedUpdate;
+				break;
+			case SegmentState.DETACHED:
+				InitDetached();
+				stateUpdate = DetachedUpdate;
+				break;
+			case SegmentState.GRABBED:
+				InitGrabbed();
+				stateUpdate = GrabbedUpdate;
+				break;
+		}
+	}
+
+	[SerializeField] ArmPullScript forwardNeighbor;
+	[SerializeField] ArmPullScript backNeighbor;
 
 	Vector3 lastMousePosition;
 	Vector3 offset;
 	Vector3 momentum;
-	[SerializeField] [Range(0,1)] float drag = 0.1f;
-
-	[SerializeField] [Range(0,2)] float force = 1f;
 
 	Vector3 startPos;
 	Vector3 startOffset;
 
 	// Use this for initialization
 	void Start () {
-		startPos = transform.position;
-		if(neighborSegment != null)
-			startOffset = transform.position - neighborSegment.transform.position;
+		SetState(currentState);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(grabbed){ //TODO: use a state machine instead
-			if(Input.GetMouseButtonUp(0)){
-				grabbed = false;
-			}
-
-			Vector3 worldSpaceMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			worldSpaceMouse.z = 0;
-
-			offset = transform.position - worldSpaceMouse;
-
-			momentum -= offset * force * Time.deltaTime;
-
-			
-
-		} else if(neighborSegment != null){
-			offset = (transform.position - neighborSegment.transform.position) - startOffset;
-			momentum -= offset * force * Time.deltaTime;
+		if(stateUpdate != null){
+			stateUpdate();
 		}
-		transform.position += momentum;
 	}
 
 	void FixedUpdate(){
-		momentum *= 1 - drag;
+		momentum *= 1 - ArmPullManager.Instance.drag;
 	}
 
+	#region ATTACHED
+	void InitAttached(){
+		startPos = transform.position;
+		if(forwardNeighbor != null)
+			startOffset = transform.position - forwardNeighbor.transform.position;
+	}
+
+	void AttachedUpdate(){
+		if(Vector3.Distance(forwardNeighbor.transform.position, transform.position) > ArmPullManager.Instance.detachDistance){
+			SetState(SegmentState.DETACHED);
+		}
+	}
+	#endregion
+
+	#region DETACHED
+	void InitDetached(){
+
+	}
+
+	void DetachedUpdate(){
+		if(forwardNeighbor != null){
+			offset = (transform.position - forwardNeighbor.transform.position) - startOffset;
+			if(offset.magnitude > ArmPullManager.Instance.maxSegmentDistance){
+				transform.position = forwardNeighbor.transform.position + offset.normalized * ArmPullManager.Instance.maxSegmentDistance;
+				momentum = Vector3.zero;
+				Debug.Log("HELL0?");
+			} else{
+				momentum -= offset * ArmPullManager.Instance.pullForce * Time.deltaTime;
+			}
+		}
+		transform.position += momentum;
+	}
+	#endregion
+
+	#region GRABBED
+	void InitGrabbed(){
+		lastMousePosition = transform.position;
+		momentum = Vector3.zero;
+	}
+
+	void GrabbedUpdate(){
+		if(Input.GetMouseButtonUp(0)){
+			SetState(SegmentState.DETACHED);
+		}
+
+		Vector3 worldSpaceMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		worldSpaceMouse.z = 0;
+
+		offset = transform.position - worldSpaceMouse;
+
+		momentum -= offset * ArmPullManager.Instance.pullForce * Time.deltaTime;
+		
+		transform.position += momentum;
+	}
+	#endregion
+
 	void OnMouseDown(){
-		if(detached){
-			grabbed = true;
-			lastMousePosition = transform.position;
-			// Vector3 worldSpaceMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			// worldSpaceMouse.z = 0;
-			// offset = transform.position - worldSpaceMouse;
+		if(currentState == SegmentState.DETACHED){
+			SetState(SegmentState.GRABBED);
 		}
 	}
 
